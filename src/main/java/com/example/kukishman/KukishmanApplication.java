1086170d9fbfa112deae158292f678e2cf6ebd78
Kusherekin Dmitriy <kusherekin@mail.ru>
2023-04-24 18:08:50 +0300

    First commit

---
+++ src/main/java/com/example/kukishman/KukishmanApplication.java
@@ -0,0 +1,14 @@
+package com.example.kukishman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukishmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KukishmanApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/kukishman/KukishmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kukishman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukishmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
